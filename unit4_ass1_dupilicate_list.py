39d19a91bd96a62a76bff3cb30f14f640d085707
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-29 10:22:37 +0530

    Add files via upload

---
+++ unit4_ass1_dupilicate_list.py
@@ -0,0 +1,56 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[13, 14, 56, 35]\n"
+     ]
+    }
+   ],
+   "source": [
+    "def remove(dup):\n",
+    "    list=[]\n",
+    "    for num in dup:\n",
+    "        if num not in list:\n",
+    "            list.append(num)\n",
+    "    return list\n",
+    "dup=[13,14,13,56,35,56]\n",
+    "print(remove(dup))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
