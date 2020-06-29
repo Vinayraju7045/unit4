c30301d7e30bd61ee257dd61b41b8bac2ab53b8c
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-29 10:18:14 +0530

    Add files via upload

---
+++ unit4_ass1_list_difference.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[10, 20, 30, 15]\n"
+     ]
+    }
+   ],
+   "source": [
+    "def Diff(l1, l2): \n",
+    "    return (list(set(l1) - set(l2))) \n",
+    "l1 = [10, 15, 20, 25, 30, 35, 40] \n",
+    "l2 = [25, 40, 35] \n",
+    "print(Diff(l1, l2))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
